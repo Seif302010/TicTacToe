091204926b158cfd47c90ffbd6ae1f601afc7d44
Seif302010 <seif302010@gmail.com>
2024-06-11 02:25:22 +0300

    build scripts updated

--- build.bat
+++ build.bat
@@ -1,4 +1,9 @@
-windres addIcon.rc -O coff -o addIcon-x64.res
-windres addIcon.rc -O coff -F pe-i386 -o addIcon-x32.res
-g++ -static -o tic_tac_toe-x64 main.cpp addIcon-x64.res
-g++ -static -m32 -o tic_tac_toe-x32 main.cpp addIcon-x32.res
+echo off
+set MINGW32=C:\MinGW-W64\mingw32\bin
+set MINGW64=C:\MinGW-W64\mingw64\bin
+echo on
+
+%MINGW64%\windres addIcon.rc -O coff -o addIcon-x64.res
+%MINGW32%\windres addIcon.rc -O coff -F pe-i386 -o addIcon-x32.res
+%MINGW64%\g++ -static -o tic_tac_toe-x64 main.cpp addIcon-x64.res
+%MINGW32%\g++ -static -m32 -o tic_tac_toe-x32 main.cpp addIcon-x32.res
